90ee457a6bea038b93ee5eb75ecc3e25becea3be
rongliang <cheng_rongliang@163.com>
2020-08-03 16:53:52 +0800

    commit changes

--- src/test/groovy/com/hexmeet/autotestcases/testsuites/PrivateCloudTestSuite.groovy
+++ src/test/groovy/com/hexmeet/autotestcases/testsuites/PrivateCloudTestSuite.groovy
@@ -1,6 +1,6 @@
 package com.hexmeet.autotestcases.testsuites
 
-
+import com.hexmeet.autotestcases.privatecloud.AppVersion
 import com.hexmeet.autotestcases.privatecloud.CallAContactInPrivate
 import com.hexmeet.autotestcases.privatecloud.GuestCall
 import com.hexmeet.autotestcases.privatecloud.JoinAReserveMeeting
@@ -17,6 +17,7 @@ import org.junit.runners.Suite
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses([
+            AppVersion,
             SignIn,
             GuestCall,
             OperateInAGuestCall,
